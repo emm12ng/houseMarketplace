537c444aed865f01b02c376c95c744b5cec4074c
emm12ng <emm12nguyen@gmail.com>
2022-06-07 19:07:46 -0400

    6/7

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
-import { Swiper, SwiperSlide } from 'swiper/react'
+'../../node_modules/swiper/react'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
